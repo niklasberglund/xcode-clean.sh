f725bcc5de3f86bcf6ea7284bcb109e8c71bfd66
niklasberglund <niklas@asdasd.net>
2016-08-24 23:52:01 +0200

    Added xcode-clean.sh with basic functionality

---
+++ xcode-clean.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+
+archives_path=~/"Library/Developer/Xcode/Archives"
+derived_data_path=~/"Library/Developer/Xcode/DerivedData"
+simulator_data_path=~/"Library/Developer/CoreSimulator/Devices/"
+
+remove_archives=false
+remove_derived_data=false
+remove_simulator_data=false
+
+remove_contents() {
+    arg_path=$1
+    arg_name=$2
+    
+    size=$(du -hcs "$arg_path" | tail -1 | cut -f1 | xargs)
+    
+    printf "Removing $arg_name in ${arg_path}* (freeing $size disk space)\n"
+    rm -R "$arg_path"/*
+}
+
+usage() {
+cat << EOF
+    Usage: $0 [options]
+
+    Frees up disk space by removing Xcode data. NOTE: you might want to keep backups of the dsym files in ~/Library/Developer/Xcode/Archives
+    
+    EXAMPLE:
+        $0 -A
+
+    OPTIONS:
+       -h      Show this help message
+       -a      Removed all Xcode archives
+       -d      Removed everything in DerivedData folder
+       -s      Remove simulator data
+       -A      Remove all of the above(archived, DerivedData and simulator data)
+
+EOF
+}
+
+while getopts "hadsA" OPTION
+do
+    case $OPTION in
+        h)
+            usage
+            exit 0
+            ;;
+        a)
+            remove_archives=true
+            ;;
+        d)
+            remove_derived_data=true
+            ;;
+        s)
+            remove_simulator_data=true
+            ;;
+        A)
+            remove_archives=true
+            remove_derived_data=true
+            remove_simulator_data=true
+            ;;
+        \?)
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+if $remove_archives; then
+    remove_contents "$archives_path" "archives"
+fi
+
+if $remove_derived_data; then
+    remove_contents "$derived_data_path" "DerivedData content"
+fi
+
+if $remove_simulator_data; then
+    remove_contents "$simulator_data_path" "simulator data"
+fi
